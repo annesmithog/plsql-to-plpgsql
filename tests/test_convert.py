a12ce2ba3114837dbfbd04b6307404434f11ede9
annesmithog <annesmithog@gmail.com>
2024-12-31 01:23:50 +0900

    modify extensions

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -5,29 +5,29 @@ def test_normal_select():
     assert run_convert(oracle) == oracle
 
 def test_from_dual():
-    with open('app/examples/oracle/from_dual.sql', 'r') as f:
+    with open('app/examples/oracle/from_dual.plb', 'r') as f:
         ora = f.read()
-    with open('app/examples/postgres/from_dual.sql', 'r') as f:
+    with open('app/examples/postgres/from_dual.pgsql', 'r') as f:
         pos = f.read()
     assert run_convert(ora) == pos
 
 def test_select1():
-    with open('app/examples/oracle/select1.sql', 'r') as f:
+    with open('app/examples/oracle/select1.plb', 'r') as f:
         ora = f.read()
-    with open('app/examples/postgres/select1.sql', 'r') as f:
+    with open('app/examples/postgres/select1.pgsql', 'r') as f:
         pos = f.read()
     assert run_convert(ora) == pos
 
 def test_basic_structure():
-    with open('app/examples/oracle/basic_structure.sql', 'r') as f:
+    with open('app/examples/oracle/basic_structure.plb', 'r') as f:
         ora = f.read()
-    with open('app/examples/postgres/basic_structure.sql', 'r') as f:
+    with open('app/examples/postgres/basic_structure.pgsql', 'r') as f:
         pos = f.read()
     assert run_convert(ora) == pos
 
 def test_declaration():
-    with open('app/examples/oracle/declaration.sql', 'r') as f:
+    with open('app/examples/oracle/declaration.plb', 'r') as f:
         ora = f.read()
-    with open('app/examples/postgres/declaration.sql', 'r') as f:
+    with open('app/examples/postgres/declaration.pgsql', 'r') as f:
         pos = f.read()
     assert run_convert(ora) == pos
